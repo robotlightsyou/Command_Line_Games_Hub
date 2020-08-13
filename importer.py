9c9cbab7e23a21d879cba9d7af6acef2f89bf8b3
Adam Earle <robotlightsyou@gmail.com>
2020-08-13 14:39:06 -0700

    importer attempts to censor definitions

--- importer.py
+++ importer.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 
+
+#@TODO: use re module to censor defs nb ATBS chapter
+#@TODO: accept command line args for __dict_name__
+
+
+import re
 import csv
 import pprint
+
 
 with open('eoslong.csv', 'r') as f:
     reader = csv.reader(f)
@@ -10,16 +17,19 @@ with open('eoslong.csv', 'r') as f:
         try:
             k,v = row[0].split(':', 1)
             #delete leading white space from definition
-            for char in v:
-                if char == ' ':
-                    v = v[1:]
-                else:
-                    break
-            v = v.replace('\n', ' ')
-            result[k] = v
         except:
             pass
-    # pprint.pprint(result)
+        for char in v:
+            if char == ' ':
+                v = v[1:]
+            else:
+                break
+        v = v.replace('\n', ' ')
+        v = v.replace("'", "\\'")
+        occurances = re.findall(k,v, re.IGNORECASE)
+        for occurance in occurances:
+            v = v.replace(occurance, "*****")
+        result[k] = v
     with open('dicts.py', 'a') as out:
         out.write('\n\nEOSDICT = {')
         for k,v in result.items():
